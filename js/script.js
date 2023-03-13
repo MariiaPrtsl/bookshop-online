f0d6f66790b058b52432726d1b62d6bef906c526
Mariia Protsailo <108431414+MariiaPrtsl@users.noreply.github.com>
2023-03-13 15:57:49 +0200

    online-shop

---
+++ js/script.js
@@ -0,0 +1,126 @@
+searchForm = document.querySelector(".search-form");
+
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.toggle("active");
+};
+
+
+
+let loginForm = document.querySelector(".login-form-container");
+document.querySelector("#login-btn").onclick = () => {
+  loginForm.classList.toggle("active");
+};
+
+document.querySelector("#close-login-btn").onclick = () => {
+  loginForm.classList.remove("active");
+};
+
+window.onscroll = () => {
+  searchForm.classList.remove("active");
+
+  if (window.scrollY > 80) {
+    document.querySelector(".header .header-2").classList.add("active");
+  } else {
+    document.querySelector(".header .header-2").classList.remove("active");
+  }
+
+  // fadeOut()
+};
+
+window.onload = () => {
+  if (window.scrollY > 80) {
+    document.querySelector(".header .header-2").classList.add("active");
+  } else {
+    document.querySelector(".header .header-2").classList.remove("active");
+  }
+};
+
+
+
+function loader() {
+  document.querySelector(".loader-container").classList.add("active");
+}
+
+// function fadeOut(){
+//     setTimeout(loader, 1000);
+// }
+
+
+
+
+
+
+
+
+
+
+const swiper1 = new Swiper(".book-slider", {
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+const swiper3 = new Swiper(".arrivals-slider", {
+  spaceBetween: 10,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+const swiper4 = new Swiper(".review-slider", {
+  spaceBetween: 10,
+  loop: true,
+  grabCursor: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+
+
